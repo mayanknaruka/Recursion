187d34995b16f2f033920f997f2dd937dc58ea73
mayanknaruka <mayanknaruka47@gmail.com>
2026-09-10 17:20:26 +0530

    solve and testing complete

--- fibonacci.js
+++ fibonacci.js
@@ -21,3 +21,37 @@ const fibs = function(number){
     return arr
 } 
 
+// recursive function 
+
+const fibsRec = function(number){
+
+    if (number == 0) { 
+        return [] 
+    }
+    if (number == 1 ){
+        return[0]
+    }
+    if(number == 2 ){
+        return [0,1]
+    }
+
+    const previous = fibsRec(number - 1)
+
+    const nextNumber = previous[previous.length - 1] + previous[previous.length - 2]
+
+    previous.push(nextNumber)
+
+    return previous
+}
+
+console.log(fibs(0))
+console.log(fibs(1))
+console.log(fibs(2))
+console.log(fibs(5))
+console.log(fibs(8))
+
+console.log(fibsRec(0))
+console.log(fibsRec(1))
+console.log(fibsRec(2))
+console.log(fibsRec(5))
+console.log(fibsRec(8))
