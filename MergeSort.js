a7352dd11a9e9d816377f70630dbd7ed92828c6e
mayanknaruka <mayanknaruka47@gmail.com>
2026-09-11 14:03:29 +0530

    all done dude

--- MergeSort.js
+++ MergeSort.js
@@ -1,3 +1,61 @@
 const mergeSort = function(array){
     
+    if(array.length <= 1){
+        return array
+    }
+
+    const middle = Math.floor(array.length / 2)
+
+    const left = array.slice(0, middle);
+    const right = array.slice(middle);
+
+    const sortLeft = mergeSort(left)
+    const sortRight = mergeSort(right)
+
+    return merge(sortLeft, sortRight)
 }
+
+const merge = function(left, right ){
+    const result =[]
+
+    let leftIndex = 0
+    let rightIndex = 0
+
+    while(leftIndex < left.length && rightIndex < right.length){
+
+        if(left[leftIndex] < right[rightIndex]){
+            result.push(left[leftIndex])
+                leftIndex++
+            } else{
+                result.push(right[rightIndex])
+                rightIndex++
+
+            }
+        }
+
+        while (leftIndex < left.length){
+            result.push(left[leftIndex])
+            leftIndex++ 
+        }
+
+        while(rightIndex < right.length){
+            result.push(right[rightIndex])
+            rightIndex++
+        }
+
+        return result
+    }
+    console.log(mergeSort([]));
+// []
+
+console.log(mergeSort([73]));
+// [73]
+
+console.log(mergeSort([1, 2, 3, 4, 5]));
+// [1, 2, 3, 4, 5]
+
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
+// [0, 1, 1, 2, 3, 5, 8, 13]
+
+console.log(mergeSort([105, 79, 100, 110]));
+// [79, 100, 105, 110]
